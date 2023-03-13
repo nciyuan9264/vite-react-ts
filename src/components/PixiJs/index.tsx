5315beb7a50eb732942fbc3cf15390810598a9e2
zhenyu <1522457543@qq.com>
2023-03-14 00:06:31 +0800

    fix: Ticker测试

--- src/components/PixiJs/index.tsx
+++ src/components/PixiJs/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useRef } from "react";
+import * as PIXI from 'pixi.js'
 import { PixiMain } from './pixiTest/PixiMain'
 import { Draw } from './pixiTest/Draw'
 const PixiJs: React.FC = () => {
     let pixiMain: any = null;
+    let draw: any = null;
     const pixiRoot = useRef<HTMLCanvasElement>(null);
     useEffect(() => {
         initPixiMain();
@@ -16,17 +18,30 @@ const PixiJs: React.FC = () => {
         pixiMain = new PixiMain(pixiRoot.current as HTMLCanvasElement);
     }
 
+    const update = (delta: number, ticker: any) => {
+        console.log(delta, ticker.deltaTime);
+    }
+
     const startDraw = () => {
-        let draw = new Draw(pixiMain);
+        draw = new Draw(pixiMain);
         draw.draw();
     }
 
     const render = () => {
-        pixiMain.renderer.render(pixiMain);
+        const ticker = new PIXI.ticker.Ticker();
+        ticker.minFPS = 60;
+        ticker.deltaTime = 1 / ticker.minFPS;
+        ticker.autoStart = true;
+        // 添加更新函数到Ticker对象
+        ticker.add((delta: number) => {
+            // pixiMain.renderer.render(pixiMain);
+            update(delta, ticker);
+        });
     }
 
     const changeCanvasSize = () => {
         pixiMain.renderer.resize(window.innerWidth, window.innerHeight);
+        pixiMain.renderer.render(pixiMain);
     }
 
     return (
--- src/components/PixiJs/pixiTest/Draw.ts
+++ src/components/PixiJs/pixiTest/Draw.ts
@@ -2,17 +2,23 @@ import * as PIXI from 'pixi.js'
 
 export class Draw extends PIXI.Container {
     root: any;
+    graphics: PIXI.Graphics;
 
     constructor(pixiMain: any) {
         super();
         this.root = pixiMain
+        this.graphics = new PIXI.Graphics();
     }
 
     draw = () => {
-        const graphics = new PIXI.Graphics();
-        graphics.beginFill(0x000fff);
-        graphics.drawRect(200, 200, 50, 50);
-        graphics.endFill();
-        this.root.addChild(graphics);
+        this.graphics.beginFill(0x000fff);
+        this.graphics.drawRect(200, 200, 50, 50);
+        this.graphics.endFill();
+        this.root.addChild(this.graphics);
+    }
+
+
+    clearDraw = () => {
+        this.graphics.clear();
     }
 }
