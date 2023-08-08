ead7afc6bc3df90efb432402c6e05e5b769f4829
ZhenyuWang <zhenyu.wang1@eeoa.com>
2023-08-08 15:19:37 +0800

    fix: 添加howler播放音频

--- src/components/VisualizingQuaternions/components/CtrlBar/index.tsx
+++ src/components/VisualizingQuaternions/components/CtrlBar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Howl, Howler } from 'howler';
 import './index.scss'
 
 // type Props = {
@@ -9,7 +10,7 @@ import './index.scss'
 const CtrlBar: React.FC = () => {
     // const { radius, setRadius } = props;
 
-    // const [isDragging, setIsDragging] = useState(false);
+    const [play, setPlay] = useState(false);
     // const [lastY, setLastY] = useState(null);
 
     // const slidingBlockRef = useRef<HTMLDivElement>(null);
@@ -18,12 +19,33 @@ const CtrlBar: React.FC = () => {
 
     useEffect(() => {
 
+
     }, []);
+
+    const sound = new Howl({
+        src: ['/3.mp3'], // 替换成您的音频文件路径
+        autoplay: false,
+        loop: false,
+        volume: 0.5, // 设置音量，范围从0到1
+    });
+
+    const playOrStop = () => {
+        if (!play) {
+            sound.play();
+        } else {
+            sound.stop();
+            console.log(123);
+        }
+
+        setPlay(!play);
+    };
 
 
     return (
         <div className='cb-root'>
-            
+            <div className='playBtn' onClick={playOrStop}>
+                {play ? <span className='stop'>{'||'}</span> : <span className='play'>{'>'}</span>}
+            </div>
         </div>
     );
 };
